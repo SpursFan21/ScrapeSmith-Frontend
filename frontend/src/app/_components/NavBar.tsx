ea95fa396d9f872db5702ce4fe4ed9f4697ed4a9
SpursFan21 <duncanfrederic@gmail.com>
2025-05-29 20:36:36 +1200

    updated logo

--- frontend/src/app/_components/NavBar.tsx
+++ frontend/src/app/_components/NavBar.tsx
@@ -4,6 +4,7 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { RootState, useAppDispatch } from "../../redux/store";
@@ -28,7 +29,6 @@ const Navbar: React.FC = () => {
     dispatch(fetchForgeBalance());
   }, [accessToken, dispatch]);
 
-
   const handleLogout = async () => {
     try {
       await api.post("/auth/logout");
@@ -43,9 +43,20 @@ const Navbar: React.FC = () => {
   return (
     <nav className="sticky top-0 z-50 w-full bg-gray-800 shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
-        <Link href="/" className="text-2xl font-bold text-amber-500">
-          ScrapeSmith
+        <Link href="/" className="flex items-center space-x-3 group">
+          <Image
+            src="/logo.svg"
+            alt="ScrapeSmith Logo"
+            width={40}
+            height={40}
+            className="h-10 w-10 transition-transform duration-200 group-hover:scale-105"
+            priority
+          />
+          <span className="text-2xl font-bold text-amber-500 group-hover:text-amber-400">
+            ScrapeSmith
+          </span>
         </Link>
+
         <div className="flex space-x-6 items-center">
           <Link href="/" className="text-white hover:text-amber-400">
             Home
