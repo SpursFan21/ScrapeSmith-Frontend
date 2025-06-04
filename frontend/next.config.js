2b883826ff15737cf43a547d75f1ec48221e8b65
SpursFan21 <duncanfrederic@gmail.com>
2025-06-04 20:28:44 +1200

    file restructured

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,5 @@
+// frontend/next.config.js
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,6 +7,8 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  // Tells Next.js to treat `src/` as the app root
+};
 
 export default config;
